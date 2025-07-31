1f261b14879bbe83a34ccee3aa3baf2a78ff7e66
jezzydev <jezuy87@gmail.com>
2025-07-31 16:57:24 +0800

    Add date and time validation

--- scripts/app.js
+++ scripts/app.js
@@ -6,6 +6,8 @@ const cateringItemSelectedClassName = 'CateringItem--selected';
 const submitBtn = document.querySelector('.ReservationSubmit__button');
 const reservationForm = document.querySelector('.ReservationForm');
 const errorFieldIsRequired = "This field is required.";
+const errorFieldIsIncomplete = "This field is incomplete.";
+const yearInput = document.querySelector('#year');
 
 cateringItemButtons.forEach(function (btn) {
     btn.addEventListener('click', function () {
@@ -44,52 +46,196 @@ reservationForm.addEventListener('submit', function (event) {
 });
 
 function validateForm(form) {
+    validateName(form);
+    validateEmail(form);
+    validateDate(form);
+    validateTime(form);
+    validateReservationTime(form);
+    //TODO: validate # people
+}
 
+function validateName(form) {
     const fullnameRegEx = /^[a-zA-Z\s',\.-]+$/;
-    const emailRegEx = /^[\w.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-    //check fullname
     const fullnameValue = form.fullname.value;
+    let errorMsgElem = null;
+
     if (fieldIsEmpty(fullnameValue)) {
-        let errorDiv = createErrorMessage(form.fullname, errorFieldIsRequired, true);
-        addErrorMessage(form.fullname, errorDiv);
+        errorMsgElem = createErrorMessage(form.fullname.name, errorFieldIsRequired, true, true);
+        // addErrorMessageAfterElem(form.fullname, errorMsgElem);
     }
     else if (!fullnameRegEx.test(fullnameValue)) {
-        let errorDiv = createErrorMessage(form.fullname, 'This field is invalid.', true);
-        addErrorMessage(form.fullname, errorDiv);
+        errorMsgElem = createErrorMessage(form.fullname.name, 'This field is invalid.', true, true);
+        // addErrorMessageAfterElem(form.fullname, errorDiv);
     }
 
-    //check email
+    addErrorMessageAfterElem(form.fullname, errorMsgElem);
+    setInvalid([form.fullname], errorMsgElem);
+}
+
+function validateEmail(form) {
+    const emailRegEx = /^[\w.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     const emailValue = form.email.value;
+    let errorMsgElem = null;
+
     if (fieldIsEmpty(emailValue)) {
-        let errorDiv = createErrorMessage(form.email, errorFieldIsRequired, true);
-        addErrorMessage(form.email, errorDiv);
+        errorMsgElem = createErrorMessage(form.email.name, errorFieldIsRequired, true, true);
+        // addErrorMessageAfterElem(form.email, errorDiv);
     }
     else if (!emailRegEx.test(emailValue)) {
-        let errorDiv = createErrorMessage(form.email, 'This email address is invalid.', true);
-        addErrorMessage(form.email, errorDiv);
+        errorMsgElem = createErrorMessage(form.email.name, 'This email address is invalid.', true, true);
+        // addErrorMessageAfterElem(form.email, errorDiv);
     }
+
+    addErrorMessageAfterElem(form.email, errorMsgElem);
+    setInvalid([form.email], errorMsgElem);
+}
+
+function validateDate(form) {
+    const monthRegEx = /^(0?[1-9]|1[0-2])$/;
+    const dayRegEx = /^(0?[1-9]|[12][0-9]|3[01])$/;
+    const yearRegEx = /^\d{4}$/;
+    const monthValue = form.month.value;
+    const dayValue = form.day.value;
+    const yearValue = form.year.value;
+    const dateLabel = document.querySelector('.Date__label');
+    const dateInputs = document.querySelectorAll('.Date__container input');
+
+    let errorMsgElem = null;
+    let incompleteDateInput = null;
+
+    //check for completeness    
+    if (fieldIsEmpty(monthValue)) {
+        incompleteDateInput = form.month;
+    }
+    else if (fieldIsEmpty(dayValue)) {
+        incompleteDateInput = form.day;
+    }
+    else if (fieldIsEmpty(yearValue)) {
+        incompleteDateInput = form.year;
+    }
+
+    if (incompleteDateInput) {
+        errorMsgElem = createErrorMessage('date', errorFieldIsIncomplete);
+        addErrorMessageAfterElem(dateLabel, errorMsgElem);
+        setInvalid(dateInputs, errorMsgElem);
+        addLabelErrorClass(dateLabel);
+
+        return;
+    }
+
+    errorMsgElem = null;
+
+    //ensure month is 1-12
+    if (!monthRegEx.test(monthValue)) {
+        errorMsgElem = createErrorMessage(form.month.name, 'Month is incorrect.');
+        setInvalid([form.month], errorMsgElem);
+    }
+    //ensure day is 1-31
+    else if (!dayRegEx.test(dayValue)) {
+        errorMsgElem = createErrorMessage(form.day.name, 'Day is incorrect.');
+        setInvalid([form.day], errorMsgElem);
+    }
+    //ensure year is not in the past
+    else if (!yearRegEx.test(yearValue)) {
+        errorMsgElem = createErrorMessage(form.year.name, 'Year is incorrect.');
+        setInvalid([form.year], errorMsgElem);
+    }
+
+    if (errorMsgElem) {
+        addErrorMessageAfterElem(dateLabel, errorMsgElem);
+        addLabelErrorClass(dateLabel);
+
+        return;
+    }
+
+    errorMsgElem = null;
+
+    //ensure date is valid, and not autocorrected by javascript
+    const tempDate = new Date(yearValue, monthValue - 1, dayValue);
+
+    if (tempDate.getFullYear() != yearValue ||
+        tempDate.getMonth() != (monthValue - 1) ||
+        tempDate.getDate() != dayValue) {
+        errorMsgElem = createErrorMessage('date', 'Date in invalid.');
+        addErrorMessageAfterElem(dateLabel, errorMsgElem);
+        setInvalid(dateInputs, errorMsgElem);
+        addLabelErrorClass(dateLabel);
+
+        return;
+    }
+}
+
+function validateTime(form) {
+    const hourRegEx = /^(0?[1-9]|1[0-2])$/;
+    const minRegEx = /^([0-5]?\d)$/;
+    const hourValue = form.hour.value;
+    const minuteValue = form.minute.value;
+    const meridiemValue = form.meridiem.value;
+    const timeInputs = document.querySelectorAll('.Time__container :is(input, select)');
+    const timeLabel = document.querySelector('.Time__label');
+
+    let errorMsgElem = null;
+    let incompleteTimeInput = null;
+
+    //check for completeness
+    if (!fieldIsEmpty(hourValue)) {
+        incompleteTimeInput = form.hour;
+    }
+    else if (!fieldIsEmpty(minuteValue)) {
+        incompleteTimeInput = form.minute;
+    }
+    else if (!fieldIsEmpty(meridiemValue)) {
+        incompleteTimeInput = form.meridiem;
+    }
+
+    if (incompleteTimeInput) {
+        errorMsgElem = createErrorMessage('time', errorFieldIsIncomplete);
+        addErrorMessageAfterElem(timeLabel, errorMsgElem);
+        setInvalid(timeInputs, errorMsgElem);
+        addLabelErrorClass(timeLabel);
+
+        return;
+    }
+}
+
+function validateReservationTime(reservationDate) {
+    let today = new Date();
+
+    //TODO: ensure reservation date is not in the past
 }
 
 function fieldIsEmpty(str) {
     return (!str || !str.replaceAll(' ', ''));
 }
 
-function addErrorMessage(elem, errorDiv) {
-    elem.setAttribute('aria-invalid', true);
-    elem.setAttribute('aria-errormessage', errorDiv.id);
-    elem.after(errorDiv);
+function setInvalid(inputList, errorMsgElem) {
+    for (let i = 0; i < inputList.length; i++) {
+        inputList[i].setAttribute('aria-invalid', true);
+        inputList[i].setAttribute('aria-errormessage', errorMsgElem.id);
+    }
 }
 
-function createErrorMessage(elem, errorMsg, withLeftPadding) {
+function addErrorMessageAfterElem(refElem, errorMsgElem) {
+    refElem.after(errorMsgElem);
+}
+
+function addLabelErrorClass(labelElem) {
+    labelElem.classList.add('Label--targetError');
+}
+
+function createErrorMessage(errorMsgPrefex, errorMsg, withLeftPadding, withTopPadding) {
     const errorMessageSuffix = '-errorMsg';
     const div = document.createElement('div');
-    div.setAttribute('id', elem.name + errorMessageSuffix);
+    div.setAttribute('id', errorMsgPrefex + errorMessageSuffix);
     div.setAttribute('role', 'alert');
     div.classList.add('FormItem__inputError');
 
     if (withLeftPadding) {
         div.classList.add('FormItem__inputError--paddingLeft');
+    }
+
+    if (withTopPadding) {
+        div.classList.add('FormItem__inputError--paddingTop');
     }
 
     div.textContent = errorMsg;
@@ -111,4 +257,9 @@ function resetValidation() {
 
     const errorMessages = document.querySelectorAll('.FormItem__inputError');
     errorMessages.forEach(error => error.remove());
+
+    const labelErrors = document.querySelectorAll('.Label--targetError');
+    labelErrors.forEach(label => {
+        label.classList.remove('Label--targetError');
+    });
 }
